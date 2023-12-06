c95d3c0cdf86b8dd63898fb1aeb9925d1411a11e
Ольга Чернецкая <chernetzkayaolga@yandex.by>
2023-12-06 16:14:29 +0300

    Add deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['olgataski.ddns.net', '158.160.8.206', '127.0.0.1', '127.0.0.1:8080', 'localhost']
+ALLOWED_HOSTS = ['olgataski.ddns.net', '158.160.8.206', '127.0.0.1', 'localhost']
 
 
 # Application definition
